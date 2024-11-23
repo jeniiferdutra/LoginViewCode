9b512783d8761fda149edf0acac80d0e5cc795b9
jenifer rocha <jeniferdutra2000@icloud.com>
2024-11-23 01:48:13 -0300

    configuracoes iniciais

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginViewCode
+//
+//  Created by Jenifer Rocha on 23/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
