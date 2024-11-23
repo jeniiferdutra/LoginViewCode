775d1cb94fca9d14ad169794791ba9d700fb7506
jenifer rocha <jeniferdutra2000@icloud.com>
2024-11-23 02:04:44 -0300

    criando o primeiro elemento

--- ViewController.swift
+++ ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var loginLabel: UILabel = { // só é executado qnd chama. util para otimizar o uso de memória ou evitar cálculos desnecessários.
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "Login"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        view.addSubview(loginLabel)
+        setUpConstraints() 
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+        
+            loginLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            
+            
+        ])
     }
 
-
+    override func viewWillAppear(_ animated: Bool) { // usado para esconder a barra de navegação assim que a tela está prestes a aparecer.
+        self.navigationController?.setNavigationBarHidden(true, animated: false) // esconde a barra de navegação.
+    }
 }
 
